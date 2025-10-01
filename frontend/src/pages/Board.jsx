58e19ffe29274ff956d22383e85e7ab6e668a4b0
abrar <abrarhameed911@gmail.com>
2025-10-01 23:39:48 +0500

    -added topbar -added styling to main board page

---
+++ frontend/src/pages/Board.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import TopBar from '@/components/TopBar'
+
+
+
+const Board = () => {
+    return (
+        <div className='w-full h-full'>
+
+            <div className="main w-full  p-5 box-border">
+                <div className="bg-[#aabeed] h-[90vh] rounded-4xl p-5">
+
+                   columns remainig
+                </div>
+            </div>
+
+        </div>
+    )
+}
+
+export default Board
